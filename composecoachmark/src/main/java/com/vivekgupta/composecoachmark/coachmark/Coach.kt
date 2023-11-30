8f4a06d1feeabbe984d53afcdacccd0c68a8e7ea
vivekgupta <itguru4all@gmail.com>
2023-11-30 08:48:18 +0530

    removed native canvas dependency and added a animation.

--- composecoachmark/src/main/java/com/vivekgupta/composecoachmark/coachmark/Coach.kt
+++ composecoachmark/src/main/java/com/vivekgupta/composecoachmark/coachmark/Coach.kt
@@ -1,13 +1,14 @@
 package com.vivekgupta.composecoachmark.coachmark
 
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,7 +30,7 @@ import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.platform.LocalDensity
@@ -38,6 +39,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
 
 /**
  *@author Vivek Gupta on 13-9-23
@@ -77,8 +79,8 @@ internal fun Coach(
     ),
     revealAnimation: RevealAnimation = RevealAnimation.RECTANGLE,
     alignment: Alignment = Alignment.BottomCenter,
-    isForcedAlignment : Boolean =false,
-    onBack : () ->Unit = {},
+    isForcedAlignment: Boolean = false,
+    onBack: () -> Unit = {},
     onSkip: () -> Unit = {},
     onNext: () -> Unit,
 ) {
@@ -86,21 +88,30 @@ internal fun Coach(
     val radius = remember {
         Animatable(0f)
     }
-
+    val focus = remember {
+            Animatable(1f)
+        }
+    val animationSpec = infiniteRepeatable<Float>(
+        animation = tween(2000, easing = FastOutSlowInEasing),
+        repeatMode = RepeatMode.Restart
+    )
     var newOffset = Offset.Zero
-    var newSize = Size(0f,0f)
+    var newSize = Size(0f, 0f)
     val rect = remember {
-        Animatable(Rect(offset = bounds.center, size = Size.Zero),rectToVector)
+        Animatable(Rect(offset = bounds.center, size = Size.Zero), rectToVector)
     }
-    when(revealAnimation){
-        RevealAnimation.CIRCLE->{
+    when (revealAnimation) {
+        RevealAnimation.CIRCLE -> {
             LaunchedEffect(key1 = bounds, block = {
                 radius.snapTo(0f)
-                val maxOf = maxOf(bounds.width,bounds.height)
-                radius.animateTo(maxOf / 2, tween(500, easing = LinearEasing))
+                val maxOf = maxOf(bounds.width, bounds.height)
+                radius.animateTo(maxOf / 2 + 20f, tween(500, easing = LinearEasing))
+                focus.snapTo(1f)
+                focus.animateTo(targetValue = 0.5f,animationSpec=animationSpec)
             })
         }
-        RevealAnimation.RECTANGLE->{
+
+        RevealAnimation.RECTANGLE -> {
             //extra padding from top to reveal view
             val x = bounds.topLeft.x - 20f
             val y = bounds.topLeft.y - 20f
@@ -110,11 +121,11 @@ internal fun Coach(
             val width = bounds.size.width + 20f
             newSize = Size(width, height)
             //new bound for the view
-            val newBound = Rect(newOffset,newSize)
+            val newBound = Rect(newOffset, newSize)
             //animating view from center
             LaunchedEffect(key1 = newBound, block = {
-                   rect.snapTo(Rect(bounds.center, size = Size.Zero))
-                    rect.animateTo(newBound, tween(500, easing = LinearEasing))
+                rect.snapTo(Rect(bounds.center, size = Size.Zero))
+                rect.animateTo(newBound, tween(500, easing = LinearEasing))
             })
         }
     }
@@ -125,7 +136,7 @@ internal fun Coach(
     }
     val offsetY = remember { Animatable(0f) }
     val alphaAnimation = remember { Animatable(0f) }
-    Surface(
+    /*Surface(
         modifier = modifier
             .fillMaxSize()
             .clickable { onNext() },
@@ -163,6 +174,127 @@ internal fun Coach(
 
                 restoreToCount(checkPoint)
             }
+        })
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            val scope = this
+
+            val canvasHeight  = with(density){
+                scope.maxHeight.roundToPx()
+            }
+            val canvasWidth = with(density){
+                scope.maxWidth.roundToPx()
+            }
+            LaunchedEffect(key1 = bounds.bottomRight.y, block = {
+                launch {
+                    offsetY.snapTo(0f)
+                    offsetY.animateTo(
+                        targetValue = bounds.bottomRight.y + distance,
+                        animationSpec = tween(
+                            durationMillis = 1000,
+                            easing = LinearOutSlowInEasing
+                        )
+                    )
+                }
+                launch {
+                    alphaAnimation.snapTo(0f)
+                    alphaAnimation.animateTo(1f, tween(1000, easing = LinearEasing))
+                }
+            })
+            CoachLayout(targetBound = bounds,
+                revealAnimation = revealAnimation,
+                canvasSize = IntSize(canvasWidth, canvasHeight),
+                alignment = alignment,
+                isForcedAlignment = isForcedAlignment,
+            ) {
+                CoachMarkMessageBox(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            alpha = alphaAnimation.value
+                        },
+                    backgroundColor = messageBoxBackgroundColor,
+                    shape = messageBoxShape,
+                    messageBoxWidth = messageBoxWidth,
+                    messageBoxHeight = messageBoxHeight
+                ) {
+                    Box(Modifier.fillMaxSize()) {
+                        Text(
+                            text = message,
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp, vertical = 10.dp)
+                                .align(Alignment.Center),
+                            style = messageBoxTextStyle,
+                            color = messageBoxTextColor
+                        )
+                    }
+                }
+            }
+
+
+            Button(
+                onClick = onSkip, modifier = skipButtonModifier
+                    .align(skipButtonAlignment),
+                colors = skipButtonColors
+            ) {
+                Text(text = skipButtonText)
+            }
+            Button(
+                onClick = onNext, modifier = nextButtonModifier
+                    .align(nextButtonAlignment),
+                colors = nextButtonColors
+            ) {
+                Text(text = nextButtonText)
+            }
+            Button(
+                onClick = onBack, modifier = backButtonModifier
+                    .align(backButtonAlignment),
+                colors = backButtonColors
+            ) {
+                Text(text = backButtonText)
+            }
+        }
+    }
+    */
+    Surface(modifier = modifier
+        .fillMaxSize()
+        .graphicsLayer(alpha = 0.99f)
+        .pointerInput(bounds) {
+            detectTapGestures {
+                onNext()
+            }
+        })
+    {
+        Canvas(modifier = Modifier, onDraw = {
+            drawRect(color = Color.Black.copy(alpha = 0.8f))
+            when (revealAnimation) {
+                RevealAnimation.CIRCLE -> {
+                    drawCircle(
+                        color = Color.White,
+                        radius = maxOf(bounds.width.absoluteValue/2,bounds.height.absoluteValue/2) *  focus.value *3f,
+                        center = bounds.center,
+                        alpha = 1- focus.value,
+                        blendMode = BlendMode.Overlay
+                    )
+                    drawCircle(
+                        color = Color.White,
+                        radius = radius.value,
+                        center=bounds.center,
+                        blendMode = BlendMode.Clear
+                    )
+
+
+                }
+
+                RevealAnimation.RECTANGLE -> {
+                    drawRect(
+                        color = Color.Unspecified,
+                        size = rect.value.size,
+                        blendMode = BlendMode.Clear,
+                        topLeft = rect.value.topLeft
+                    )
+
+                }
+            }
+
         })
         BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
             val scope = this
