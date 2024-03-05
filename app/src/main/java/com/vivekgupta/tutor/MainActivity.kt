8a623803582183005e99f3465d1a9f687bfbd521
vivekgupta <itguru4all@gmail.com>
2024-03-05 09:55:52 +0530

    added version catalog

--- app/src/main/java/com/vivekgupta/tutor/MainActivity.kt
+++ app/src/main/java/com/vivekgupta/tutor/MainActivity.kt
@@ -16,11 +16,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
--- composecoachmark/src/main/java/com/vivekgupta/composecoachmark/coachmark/CoachMarkState.kt
+++ composecoachmark/src/main/java/com/vivekgupta/composecoachmark/coachmark/CoachMarkState.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
